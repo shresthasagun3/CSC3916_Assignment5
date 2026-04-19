e14600c00eb83bdbeb69ad4836e9953476e731fd
shresthasagun3 <ssagun336@gmail.com>
2026-04-19 03:55:23 -0600

    creation of movie list sign up sign in  and code in app js

--- csc3916_assignment5/src/App.js
+++ csc3916_assignment5/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/movies" element={<MovieList />} />
+        <Route path="/movies/:title" element={<MovieDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
